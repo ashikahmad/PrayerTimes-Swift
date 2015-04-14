5567b14c2ab027a65fbafbfb653e822be2858031
Ashik Uddin Ahmad <ashikcu@gmail.com>
2015-04-14 23:55:25 +0600

    Initial Commit

---
+++ PrayerKitTests/PrayerKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  PrayerKitTests.swift
+//  PrayerKitTests
+//
+//  Created by Ashik Ahmad on 4/14/15.
+//  Copyright (c) 2015 WNeeds. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PrayerKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
